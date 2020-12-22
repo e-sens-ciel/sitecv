315061348f7c84b3f1c506dd3dddfa617389e897
e-sens-ciel <zulikhan.usumova@gmail.com>
2020-12-22 22:01:04 +0100

    scrollmagic

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 import './style.scss';
 /* Code for the signature. */
+import ScrollMagic from 'scrollmagic';
+
+const controller = new ScrollMagic.Controller();
+new ScrollMagic.Scene({
+  triggerElement: '#trigger1',
+  triggerHook: 0.9, // show, when scrolled 10% into view
+  duration: '80%', // hide 10% before exiting view (80% + 10% from bottom)
+  offset: 50, // move trigger to center of element
+})
+  .setClassToggle('#reveal1', 'visible') // add class to reveal
+  .addTo(controller);
