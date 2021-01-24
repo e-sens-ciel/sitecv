b42234447ae0875393023e5fde2598d0860bbf51
e-sens-ciel <zulikhan.usumova@gmail.com>
2021-01-24 15:42:23 +0100

    svg

--- script.js
+++ script.js
@@ -2,6 +2,31 @@ import './style.scss';
 /* Code for the signature. */
 import ScrollMagic from 'scrollmagic';
 
+// SVG ANIMATION
+function setTextAnimation(
+  delay,
+  duration,
+  strokeWidth,
+  timingFunction,
+  strokeColor,
+  repeat,
+) {
+  const paths = document.querySelectorAll('path');
+  const mode = repeat ? 'infinite' : 'forwards';
+  for (let i = 0; i < paths.length; i++) {
+    const path = paths[i];
+    const length = path.getTotalLength();
+    path.style['stroke-dashoffset'] = `${length}px`;
+    path.style['stroke-dasharray'] = `${length}px`;
+    path.style['stroke-width'] = `${strokeWidth}px`;
+    path.style.stroke = `${strokeColor}`;
+    path.style.animation = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+    path.style['animation-delay'] = `${i * delay}s`;
+  }
+}
+setTextAnimation(0.1, 3.5, 2, 'linear', '#ffffff', true);
+// END OF SVG ANIMATION
+
 const controller = new ScrollMagic.Controller();
 new ScrollMagic.Scene({
   triggerElement: '#trigger1',
